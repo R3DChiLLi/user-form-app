df6f5a93820785ee1bea4da4021a7de7c4c7eef8
R3DChiLLi <112655230+R3DChiLLi@users.noreply.github.com>
2024-12-23 21:58:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,33 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
 const cors = require('cors');
+const multer = require('multer');
 const PORT = 3000;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Initialize AWS S3
 const s3 = new AWS.S3();
 
-app.post('/submit', (req, res) => {
-  const userData = req.body; // { name: "John" }
+// Set up multer storage configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
 
   const params = {
     Bucket: 'project-bucket-for-json-file',
-    Key: `data/${Date.now()}.json`, // Store data with a unique filename (timestamp)
-    Body: JSON.stringify(userData),
-    ContentType: 'application/json',
+    Key: `data/${Date.now()}_${file.originalname}`, // Generate unique key for the file
+    Body: file.buffer,
+    ContentType: file.mimetype,
   };
 
   s3.putObject(params, (err, data) => {
     if (err) {
-      res.status(500).send('Error uploading data');
+      return res.status(500).send('Error uploading file');
     } else {
-      res.status(200).send('Data uploaded successfully');
+      return res.status(200).send('File uploaded successfully');
     }
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
