3bf611518498bd1eda7c1a43a6b8c8a925bd298a
R3DChiLLi <zahankhurram45@gmail.com>
2025-02-12 16:47:49 +0500

    .

--- frontend/change-pub-ip.sh
+++ frontend/change-pub-ip.sh
@@ -4,7 +4,7 @@
 current_ip=$(curl -s ifconfig.me)
 
 # File path of index.html
-file_path="/home/ec2-user/user-form-app/nginx.conf"
+file_path="/home/ec2-user/user-form-app/frontend/nginx.conf"
 
 # Update the IP in the fetch URL using sed
 sed -i "s|oo[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}oo|${current_ip}|g" "$file_path"
