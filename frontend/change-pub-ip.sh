188cdf7e9d2aaa489c692431df60f02b6b94bd75
R3DChiLLi <zahankhurram45@gmail.com>
2025-02-12 20:28:00 +0500

    .

--- frontend/change-pub-ip.sh
+++ frontend/change-pub-ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the current public IPv4 address
-current_ip=$(curl -s ifconfig.me)
+current_ip=$(aws elbv2 describe-load-balancers | jq -r '.LoadBalancers[0].DNSName')
 
 # File path of index.html
 file_path="/home/ec2-user/user-form-app/frontend/nginx.conf"
