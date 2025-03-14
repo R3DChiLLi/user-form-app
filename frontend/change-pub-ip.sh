951c5602966b3bd523c6001dccd946713822adef
R3DChiLLi <zahankhurram45@gmail.com>
2025-03-14 12:19:55 +0500

    Added Workflow

--- frontend/change-pub-ip.sh
+++ frontend/change-pub-ip.sh
@@ -1,7 +1,23 @@
+# #!/bin/bash
+
+# # Get the current public IPv4 address
+# current_ip=$(aws elbv2 describe-load-balancers | jq -r '.LoadBalancers[0].DNSName')
+
+# # File path of index.html
+# file_path="frontend/nginx.conf"
+
+# # Update the IP in the fetch URL using sed
+# sed -i "s|###enterALBDNShere###|${current_ip}|g" "$file_path"
+
+
+
+
+########## for running on only ec2 instance docker ############
+
 #!/bin/bash
 
 # Get the current public IPv4 address
-current_ip=$(aws elbv2 describe-load-balancers | jq -r '.LoadBalancers[0].DNSName')
+current_ip=$(curl ifconfig.me)
 
 # File path of index.html
 file_path="frontend/nginx.conf"
